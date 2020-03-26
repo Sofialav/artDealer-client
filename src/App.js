37bc8b0f506ac5e8b0210e71a7654bd2e399ebe7
Sofialav <cemeteryvixen@gmail.com>
2020-03-26 10:31:43 +0100

    Setup store, browserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import store from "./store/store";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hi!</h1>
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1>Hi!</h1>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/store/actions.js
@@ -0,0 +1,2 @@
+import superagent from "superagent";
+const baseUrl = "http://localhost:4000";
