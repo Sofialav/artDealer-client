8dad554bdb11a56231c5af5b11fbc045f96be0d4
Sofialav <cemeteryvixen@gmail.com>
2020-03-26 15:05:57 +0100

    Add buttons to artists & artworks

--- src/components/MainPageArtists.js
+++ src/components/MainPageArtists.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class MainPageArtists extends Component {
   artists = () => {
@@ -18,6 +19,9 @@ class MainPageArtists extends Component {
         <section>
           <h2>Latest artists</h2>
           {artist}
+          <Link to="/artists">
+            <button>All artists</button>
+          </Link>
         </section>
       );
     }
--- src/components/MainPageWorks.js
+++ src/components/MainPageWorks.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class MainPageWorks extends Component {
   works = () => {
@@ -15,6 +16,9 @@ class MainPageWorks extends Component {
         <section>
           <h2>Latest works</h2>
           {work}
+          <Link to="/artworks">
+            <button>All artworks</button>
+          </Link>
         </section>
       );
     }
