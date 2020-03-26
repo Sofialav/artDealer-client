de52b0476fba0498883e4c1b263856ab197d5783
Sofialav <cemeteryvixen@gmail.com>
2020-03-26 15:55:06 +0100

    Add AuthFormContainer, LoginForm with render

---
+++ src/components/authorization/AuthFormContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import LoginFormContainer from "./LoginFormContainer";
+
+class AuthFormContainer extends Component {
+  render() {
+    return (
+      <div>
+        <LoginFormContainer />
+      </div>
+    );
+  }
+}
+
+export default AuthFormContainer;
---
+++ src/components/authorization/LoginFormContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import LoginForm from "./AuthForm";
+import { connect } from "react-redux";
+
+class LoginFormContainer extends Component {
+  state = { login: "", password: "" };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.login(this.state, this.props.history);
+    this.setState({ name: "", password: "" });
+  };
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>LOGIN</h3>
+        <LoginForm
+          onSubmit={this.onSubmit}
+          onChange={this.onChange}
+          values={this.state}
+          buttonName="Login"
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(null)(LoginFormContainer);
